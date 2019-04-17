70268b0f97dcf34351b2f22cba27b712f1339550
Joy Karmoker <joykarmoker353@gmail.com>
2019-04-18 00:42:16 +0600

    added first input

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 char matrix[3][3] ={'1', '2', '3', '4', '5', '6', '7', '8', '9',};
+char player = 'x';
 void draw()
 {
+    printf("Tic tac toe\n");
     for(int i=0; i<3; i++)
     {
         for(int j=0;j<3; j++)
@@ -10,6 +13,48 @@ void draw()
             cout <<matrix[i][j] <<" ";
         }
         cout << endl;
+    }
+}
+void input()
+{
+    int first;
+    printf("Which position do you want to mark?");
+    scanf("%d", &first);
+    if(first == 1)
+    {
+        matrix[0][0] = player;
+    }
+    else if(first == 2)
+    {
+        matrix[0][1] = player;
+    }
+    else if(first == 3)
+    {
+        matrix[0][2] = player;
+    }
+    else if(first == 4)
+    {
+        matrix[1][0] = player;
+    }
+    else if(first == 5)
+    {
+        matrix[1][1] = player;
+    }
+    else if(first == 6)
+    {
+        matrix[1][2] = player;
+    }
+    else if(first == 7)
+    {
+        matrix[2][0] = player;
+    }
+    else if(first == 8)
+    {
+        matrix[2][1] = player;
+    }
+    else if(first == 9)
+    {
+        matrix[2][2] = player;
     }
 }
 
